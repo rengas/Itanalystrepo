6eae6f951af24d1ab1daa65e58e58928478f993b
rengaswamy <rengaswamy@outlook.com>
2018-09-17 10:36:35 +0800

    add comment on file as answer for 1.a.  How did you unit test it?

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class AnimalTest {
+    /*
+    1 a. How did you unit test it?
+        Unit testing primary done on methods that change the state of the object.
+        In the given classes walk,fly and sing are only creating side effects as the return type is
+        void. It is only wise to unit test methods that return something, which
+        can help us determine whether the state of the object has changed.
+     */
 }
