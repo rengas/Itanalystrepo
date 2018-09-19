350fb3e06c2594d462f00c940c94c438bc4e0b03
rengaswamy <rengaswamy@outlook.com>
2018-09-19 08:58:09 +0800

    Fix typo error

--- src/main/java/Insect.java
+++ src/main/java/Insect.java
@@ -2,7 +2,7 @@ public class Insect extends Animal {
     public Insect(String name,String sound,Flys flys,Walks walks){
         super();
         setName(name);
-        setSound("sound");
+        setSound(sound);
         setFlyingAbility(flys);
         setWalkAbility(walks);
     }
